8b658488f1725244bce82676455b9a558dd86f8e
tibesnoff <tibesnoff@gmail.com>
2024-03-12 00:33:24 -0400

    Add new files and update existing files

---
+++ flatcraft/Game.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameNamespace
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch? spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // Initialize game components, load content, etc.            
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Load game content (textures, sounds, etc.) here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // Update game logic here
+            // For example:
+            // if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //     Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Draw game objects here
+            if(spriteBatch != null){
+                spriteBatch.Begin();
+                // Draw sprites, text, etc.
+                spriteBatch.End();
+
+                base.Draw(gameTime);
+            }
+        }
+    }
+}
---
+++ flatcraft/Main.cs
@@ -0,0 +1,10 @@
+using GameNamespace;
+class Program{
+    static void Main(string[] args)
+    {
+        using (var game = new Game1())
+        {
+            game.Run();
+        }
+    }
+}
